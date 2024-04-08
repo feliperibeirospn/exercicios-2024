dcfc0c997cd79196df34053643a1fb9bec39f0de
felipe <felipecleones999@gmail.com>
2024-04-08 13:22:16 -0300

    perguntas dinamicas

--- ts/src/app/components/content/content.component.ts
+++ ts/src/app/components/content/content.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,OnInit  } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-content',
@@ -9,8 +10,33 @@ export class ContentComponent{
   informacaoExibida: string = "Informações iniciais";
   mostrarPrimeiroContainer: boolean = true;
   mostrarInformacoes: boolean = false; // Adiciona essa linha
+  isExpandido: boolean = false;
+  perguntas: any[] = [
+    {
+      assunto: 'Assunto da pergunta 1',
+      autor: 'Autor 1',
+      conteudo: 'Conteúdo da pergunta 1',
+      likes: 5,
+      respostas: 2
+    },
+    {
+      assunto: 'Assunto da pergunta 2',
+      autor: 'Autor 2',
+      conteudo: 'Conteúdo da pergunta 2',
+      likes: 3,
+      respostas: 1
+    },
+    // Adicione mais perguntas conforme necessário
+  ];
 
   toggleInformacao() {
     this.mostrarPrimeiroContainer = !this.mostrarPrimeiroContainer;
   }
+  toggleExpandido() {
+    this.isExpandido = !this.isExpandido;
+  }
+
+
+
+
 }
